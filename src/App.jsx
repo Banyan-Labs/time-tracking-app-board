c983f26f31c3aae9ae20bb174d63e567aae3355b
Josh McLain <joshmclain45@gmail.com>
2021-08-31 12:28:15 -0700

    ttab14 created context

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import AppRoute from './routes/AppRoute';
+import { AuthProvider } from './Context/AuthContext';
 import routes from './routes';
 
 const App = () => (
-  <Router>
-    <Switch>
-      {routes.map((route) => (
-        <AppRoute
-          isProtected={route.isProtected}
-          exact
-          key={uuid()}
-          path={route.path}
-          component={route.component}
-        />
-      ))}
-    </Switch>
-  </Router>
+  <AuthProvider>
+    <Router>
+      <Switch>
+        {routes.map((route) => (
+          <AppRoute
+            isProtected={route.isProtected}
+            exact
+            key={uuid()}
+            path={route.path}
+            component={route.component}
+          />
+        ))}
+      </Switch>
+    </Router>
+  </AuthProvider>
 );
 
 export default App;
--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,25 +1,17 @@
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext();
 
-export const AuthProvider = function({ children }) {
-    const [currentUser, setCurrentUser] = useState(null);
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isAuth, setIsAuth] = useState(true);
 
-    // const setAuth = (data) => {
-    //     sessionStorage.setItem('username', data.user.username);
-    //     sessionStorage.setItem('isAdmin', data.user.isAdmin);
-    //     setCurrentUser(data.user);
-    // }
+  const state = {
+    currentUser,
+    setCurrentUser,
+    isAuth,
+    setIsAuth,
+  };
 
-    return (
-        <AuthContext.Provider 
-        value={{ currentUser, setCurrentUser }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
-}
-
-export function useAuth(user) {
-    return useContext(AuthContext);
-}
+  return <AuthContext.Provider value={state}>{children}</AuthContext.Provider>;
+};
