c39590debd630eaad1d6288da50ae3f0b1c0adc9
AshleyThomas87 <77860087+AshleyThomas87@users.noreply.github.com>
2021-09-08 08:59:44 -0700

    footer update

--- src/Components/Footer/style.jsx
+++ src/Components/Footer/style.jsx
@@ -1,25 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: absolute;
+  position: fixed;
   bottom: 0;
   left: 0;
   width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: burlywood;
-  padding: 25px 15px;
+  background: #030305;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
   max-height: 30px;
 `;
 
 export const AppTitle = styled.div`
-  font-size: 22px;
+  font-family: 'Roboto';
   font-weight: 400;
+  color: #fefefe;
+  font-size: clamp(18px, 4vw, 18px);
   cursor: pointer;
 `;
 export const CompanyWrapper = styled.div`
-  font-size: 18px;
+  font-family: 'Roboto';
   font-weight: 500;
+  color: #fefefe;
+  font-size: clamp(18px, 4vw, 18px);
   cursor: pointer;
 `;
