354377e0a86657a2f8d5ccbf2d78b74610b42e9f
Joshua-Beets <86432668+Joshua-Beets@users.noreply.github.com>
2021-09-13 10:00:47 -0700

    Update index.jsx

--- src/Pages/DashboardPage/index.jsx
+++ src/Pages/DashboardPage/index.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import { PageContainer } from './style';
 
 const DashboardPage = () => {
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   user = {
+  //     emailAddress,
+  //     password,
+  //   };
+  //   axios
+  //     .post('http://127.0.0.1:8080/api/test', user)
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log('error', err);
+  //     });
+  // };
+
   return (
     <PageContainer>
       <h1>Dashboard Page</h1>
