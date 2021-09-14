137303af60a2019d79afd7d4a041ccd6efb0dab2
Joshua-Beets <86432668+Joshua-Beets@users.noreply.github.com>
2021-09-14 06:34:18 -0700

    getting and rendering data

--- src/Pages/DashboardPage/index.jsx
+++ src/Pages/DashboardPage/index.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
-import { PageContainer } from './style';
-
+import React, { useEffect, useState } from 'react';
+import { PageContainer, Card, DateColumn, ProjectColumn, Time } from './style';
+import axios from 'axios';
+import { v4 as uuid } from 'uuid';
 const DashboardPage = () => {
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   user = {
-  //     emailAddress,
-  //     password,
-  //   };
-  //   axios
-  //     .post('http://127.0.0.1:8080/api/test', user)
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((err) => {
-  //       console.log('error', err);
-  //     });
-  // };
+  const [timeEntrys, setTimeEntrys] = useState([]);
 
-  return (
-    <PageContainer>
-      <h1>Dashboard Page</h1>
-    </PageContainer>
-  );
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8080/api/timeData')
+      .then((res) => {
+        setTimeEntrys(res.data.userTimeEntry);
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  }, []);
+
+  // console.log(timeEntrys)
+
+  const workedHourList = timeEntrys.map((timeEntry) => (
+    <div key={uuid()}>
+      <Card>
+        <DateColumn>{timeEntry.date}</DateColumn>
+        <ProjectColumn>
+          {timeEntry.project}
+          <Time>{timeEntry.timeIn}</Time>
+          <Time>{timeEntry.timeOut}</Time>
+        </ProjectColumn>
+      </Card>
+    </div>
+  ));
+
+  var sortedProjects = workedHourList
+    .sort((a, b) => {
+      return (
+        new Date(a.scheduled_for).getTime() -
+        new Date(b.scheduled_for).getTime()
+      );
+    })
+    .reverse();
+
+  return <PageContainer>{sortedProjects}</PageContainer>;
 };
 
 export default DashboardPage;
--- src/Pages/DashboardPage/style.js
+++ src/Pages/DashboardPage/style.js
@@ -3,4 +3,40 @@ import styled from 'styled-components';
 export const PageContainer = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-end;
+`;
+
+export const Card = styled.div`
+  width: 80vw;
+  min-height: 25vh;
+  height: auto;
+  border: solid black 2px;
+  margin: 10px 10px 0 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const DateColumn = styled.div`
+  width: 100%;
+  height: 20%;
+  /* border-top: solid black 2px; */
+`;
+
+export const ProjectColumn = styled.div`
+  width: 100%;
+  height: 20%;
+  border-top: solid black 2px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+export const Time = styled.div`
+  /* width: 100%; */
+  margin: 0px 15px 0px 0px;
+  height: 20%;
 `;
