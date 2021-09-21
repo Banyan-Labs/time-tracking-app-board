1175a7345726e4a68ba637683a1d56dca8c3184c
Joshua-Beets <86432668+Joshua-Beets@users.noreply.github.com>
2021-09-21 09:16:29 -0700

    Auth0

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import {
@@ -14,14 +14,23 @@ import {
 } from './style';
 import { AuthContext } from '../../Context/AuthContext';
 import Image from '../../resources/images/clockLogo1.png';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
+  const { logout, loginWithRedirect, isAuthenticated } = useAuth0();
   const store = useContext(AuthContext);
+  store.setIsAuth(isAuthenticated);
+  console.log(store.isAuth, 'looking at Auth');
   const [isMobile] = useState(store.isMobile);
   const [isOpen, setIsOpen] = useState(false);
   const [navLinks] = useState(
     store.isAuth ? ['/dashboard'] : ['/sign-in', '/sign-up']
   );
+
+  useEffect(() => {
+    if (store.isAuth) history.push('/dashboard');
+  }, []);
+
   const history = useHistory();
 
   const toggleIsOpen = () => setIsOpen(!isOpen);
@@ -34,13 +43,28 @@ const Navbar = () => {
       </LogoWrapper>
       {!isMobile ? (
         <NavLinksWrapper>
-          {navLinks.map((link) => (
-            <NavLinkItem to={link} key={uuid()}>
-              {link.replace(/./g, (x, i) =>
-                /\//.test(x) ? '' : i === 1 ? x.toUpperCase() : x
-              )}
+          {navLinks.map(
+            (link) =>
+              !isAuthenticated && (
+                <NavLinkItem
+                  to={link === '/dashboard' ? link : ''}
+                  key={uuid()}
+                  onClick={() =>
+                    link !== '/dashboard' ? loginWithRedirect() : null
+                  }
+                >
+                  {link.replace(/./g, (x, i) =>
+                    /\//.test(x) ? '' : i === 1 ? x.toUpperCase() : x
+                  )}
+                </NavLinkItem>
+              )
+          )}
+
+          {isAuthenticated && (
+            <NavLinkItem to={'/logout'} onClick={() => logout()}>
+              Logout
             </NavLinkItem>
-          ))}
+          )}
         </NavLinksWrapper>
       ) : (
         <MobileNavMenuButton onClick={toggleIsOpen} />
@@ -55,6 +79,11 @@ const Navbar = () => {
               )}
             </NavLinkItem>
           ))}
+          {isAuthenticated && (
+            <NavLinkItem to={'/logout'} onClick={() => logout()}>
+              Logout
+            </NavLinkItem>
+          )}
         </MobileLinksLayout>
       )}
     </NavbarContainer>
